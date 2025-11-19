4b6f06335ef2ebe0a73fc1d42b298950bf66d259
misi9170 <michael.sinner@nrel.gov>
2025-11-19 10:57:15 -0700

    Reorganize battery_market_revenue_control example

--- examples/battery_market_revenue_control/runscript.py
+++ examples/battery_market_revenue_control/runscript.py
@@ -1,25 +1,29 @@
 # Requires Hercules v2
 
+import matplotlib.pyplot as plt
 import pandas as pd
 from hercules.grid.grid_utilities import (
     generate_locational_marginal_price_dataframe_from_gridstatus,
 )
 from hercules.hercules_model import HerculesModel
 from hercules.utilities_examples import prepare_output_directory
+from plot_outputs import plot_outputs
 from whoc.controllers import BatteryPriceSOCController, HybridSupervisoryControllerMultiRef
 from whoc.interfaces import HerculesInterface
+
+generate_output_plots = True
 
 prepare_output_directory()
 
 # Generate the LMP data needed for the simulation
 df_lmp = generate_locational_marginal_price_dataframe_from_gridstatus(
-    pd.read_csv("inputs/lmp_da.csv"),
-    pd.read_csv("inputs/lmp_rt.csv")
+    pd.read_csv("../example_inputs/lmp_da.csv"),
+    pd.read_csv("../example_inputs/lmp_rt.csv")
 )
 df_lmp.to_csv("lmp_data.csv", index=False)
 
 # Load the input file and establish the Hercules model
-hmodel = HerculesModel("inputs/hercules_input.yaml")
+hmodel = HerculesModel("hercules_input.yaml")
 
 # Establish the interface and controller, assign to the Hercules model
 interface=HerculesInterface(hmodel.h_dict)
@@ -36,3 +40,7 @@ hmodel.assign_controller(controller)
 hmodel.run()
 
 hmodel.logger.info("Process completed successfully")
+
+if generate_output_plots:
+    plot_outputs()
+    plt.show()

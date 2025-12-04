d1d2f571884421efbc0b835aafdb3bcf618b50ee
misi9170 <michael.sinner@nrel.gov>
2025-12-04 11:38:06 -0700

    Ruff formatting

--- hycon/controllers/__init__.py
+++ hycon/controllers/__init__.py
@@ -8,7 +8,9 @@ from hycon.controllers.hybrid_supervisory_controller import (
     HybridSupervisoryControllerMultiRef,
 )
 from hycon.controllers.hydrogen_plant_controller import HydrogenPlantController
-from hycon.controllers.lookup_based_wake_steering_controller import LookupBasedWakeSteeringController
+from hycon.controllers.lookup_based_wake_steering_controller import (
+    LookupBasedWakeSteeringController,
+)
 from hycon.controllers.solar_passthrough_controller import SolarPassthroughController
 from hycon.controllers.wake_steering_rosco_standin import WakeSteeringROSCOStandin
 from hycon.controllers.wind_farm_power_tracking_controller import (
